d6d831002af5be5569ab7d30f293bc47c00a2bad
matt smith <msmith21794@gmail.com>
2025-01-14 08:11:58 -0500

    removed unused guides section for now

--- astro.config.mjs
+++ astro.config.mjs
@@ -156,13 +156,13 @@ export default defineConfig({
 						},
 					],
 				},
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
+				// {
+				// 	label: 'Guides',
+				// 	items: [
+				// 		// Each item here is one entry in the navigation menu.
+				// 		{ label: 'Example Guide', slug: 'guides/example' },
+				// 	],
+				// },
 			],
 		}),
 	],
